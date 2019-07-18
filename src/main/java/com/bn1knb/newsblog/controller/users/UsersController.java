91fc7d124c0e0a39df46dbc2919e0658ac3f7762
Anton <tonykomik@gmail.com>
2019-07-18 13:10:32 +0300

    pageble getAllUsers

--- src/main/java/com/bn1knb/newsblog/controller/users/UsersController.java
+++ src/main/java/com/bn1knb/newsblog/controller/users/UsersController.java
@@ -5,7 +5,8 @@ import com.bn1knb.newsblog.model.dto.UserRegistrationDto;
 import com.bn1knb.newsblog.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -20,7 +21,6 @@ import javax.validation.constraints.Min;
 public class UsersController {
 
     private final UserService userService;
-    private static final int DATA_PER_PAGE = 5;
 
     @Autowired
     public UsersController(UserService userService) {
@@ -33,11 +33,8 @@ public class UsersController {
     }
 
     @GetMapping
-    public Page<User> getAllUsers(@RequestParam(value = "page", required = false) Integer page) {
-        if (page == null) {
-            page = 0;
-        }
-        return userService.findAllPerPage(PageRequest.of(page, DATA_PER_PAGE));
+    public Page<User> getAllUsers(@PageableDefault(size = 5) Pageable pageable) {
+        return userService.findAllPerPage(pageable);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
